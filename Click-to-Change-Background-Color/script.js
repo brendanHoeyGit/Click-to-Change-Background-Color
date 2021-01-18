f769ed36dc6e220247adde8b76896834cc8c3a0a
brendanHoeyGit <65587457+brendanHoeyGit@users.noreply.github.com>
2021-01-18 22:23:10 +0800

    Delete Click-to-Change-Background-Color directory

--- Click-to-Change-Background-Color/script.js
+++
@@ -1,9 +0,0 @@
-let colors = ['blue', 'purple', 'green', 'red', 'pink', 'yellow'];
-let button = document.getElementById('button');
-
-button.addEventListener('click', function(){
-    let random = colors[Math.floor(Math.random() * colors.length)];
-    let container = document.getElementById('container');
-
-    container.style.background = random;
-})
